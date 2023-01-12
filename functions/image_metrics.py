ad7fafeef8c059bdb385ecd9a57da88145e1120f
evihuijben <e.m.c.huijben@tue.nl>
2023-01-12 13:54:15 +0100

    updated to classes

--- functions/image_metrics.py
+++ functions/image_metrics.py
@@ -6,101 +6,114 @@ from typing import Optional
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
 
-def mae(ct: np.ndarray, 
-        sct: np.ndarray,
-        mask: Optional[np.ndarray] = None) -> float:
-    """
-    Compute Mean Absolute Error (MAE)
-
-    Parameters
-    ----------
-    ct : np.ndarray
-        Ground truth (real CT).
-    sct : np.ndarray
-        Prediction (synthetic CT).
-    mask : np.ndarray, optional
-        Mask for voxels to include. The default is None (including all voxels).
-
-    Returns
-    -------
-    mae : float
-        mean absolute error.
-
-    """
-    if mask == None:
-        mask = np.ones(ct.shape)
-    else:
-        #binarize mask
-        mask = np.where(mask>0, 1., 0.)
-        
-    mae_value = np.sum(np.abs(ct*mask - sct*mask))/mask.sum() 
-    return float(mae_value)
-
-
-def psnr(ct: np.ndarray, 
-         sct: np.ndarray,  
-         dynamic_range: Optional[int] = None,
-         mask: Optional[np.ndarray] = None) -> float:
-    """
-    Compute Peak Signal to Noise Ratio metric (PSNR)
-
-    Parameters
-    ----------
-    ct : np.ndarray
-        Ground truth (real CT).
-    sct : np.ndarray
-        Prediction (synthetic CT).
-    dynamic_range : int
-        Data range
-    mask : np.ndarray, optional
-        Mask for voxels to include. The default is None (including all voxels).
-
-    Returns
-    -------
-    psnr : float
-        Peak signal to noise ratio..
-
-    """
-    if mask == None:
-        mask = np.ones(ct.shape)
-    else:
-        #binarize mask
-        mask = np.where(mask>0, 1., 0.)
-        
-    if dynamic_range == None:
-        dynamic_range = ct.max()-ct.min()
-        
-    # apply mask
-    ct = ct[mask==1]
-    sct = sct[mask==1]
-    psnr_value = peak_signal_noise_ratio(ct, sct, data_range=dynamic_range)
-    return float(psnr_value)
-
-
-def ssim(ct: np.ndarray, 
-         sct: np.ndarray,
-         dynamic_range: Optional[int] = None) -> float:
-    """
-    Compute Structural Similarity Index Metric (SSIM)
-
-    Parameters
-    ----------
-    ct : np.ndarray
-        Ground truth (real CT).
-    sct : np.ndarray
-        Prediction (synthetic CT).
-    dynamic_range : int
-        Data range
-
-    Returns
-    -------
-    ssim : float
-        structural similarity index metric.
-
-    """
-    if dynamic_range == None:
-        dynamic_range = ct.max()-ct.min()
-        
-    ssim_value = structural_similarity(ct, sct, data_range=dynamic_range)
-    return float(ssim_value)
-
+class ImageMetrics():
+    def __init__(self):
+        # TODO
+        # Use population wide dynamic range
+        self.dynamic_range=2000 
+    
+    def mae(self,
+            gt: np.ndarray, 
+            pred: np.ndarray,
+            mask: Optional[np.ndarray] = None) -> float:
+        """
+        Compute Mean Absolute Error (MAE)
+    
+        Parameters
+        ----------
+        gt : np.ndarray
+            Ground truth
+        pred : np.ndarray
+            Prediction
+        mask : np.ndarray, optional
+            Mask for voxels to include. The default is None (including all voxels).
+    
+        Returns
+        -------
+        mae : float
+            mean absolute error.
+    
+        """
+        if mask is None:
+            mask = np.ones(gt.shape)
+        else:
+            #binarize mask
+            mask = np.where(mask>0, 1., 0.)
+            
+        mae_value = np.sum(np.abs(gt*mask - pred*mask))/mask.sum() 
+        return float(mae_value)
+    
+    
+    def psnr(self,
+             gt: np.ndarray, 
+             pred: np.ndarray,
+             mask: Optional[np.ndarray] = None,
+             use_population_range: Optional[bool] = False) -> float:
+        """
+        Compute Peak Signal to Noise Ratio metric (PSNR)
+    
+        Parameters
+        ----------
+        gt : np.ndarray
+            Ground truth
+        pred : np.ndarray
+            Prediction
+        mask : np.ndarray, optional
+            Mask for voxels to include. The default is None (including all voxels).
+        use_population_range : bool, optional
+            When a predefined population wide dynamic range should be used.
+    
+        Returns
+        -------
+        psnr : float
+            Peak signal to noise ratio..
+    
+        """
+        if mask is None:
+            mask = np.ones(gt.shape)
+        else:
+            #binarize mask
+            mask = np.where(mask>0, 1., 0.)
+            
+        if use_population_range:
+            dynamic_range = self.dynamic_range
+        else:
+            dynamic_range = gt.max()-gt.min()
+            
+        # apply mask
+        gt = gt[mask==1]
+        pred = pred[mask==1]
+        psnr_value = peak_signal_noise_ratio(gt, pred, data_range=dynamic_range)
+        return float(psnr_value)
+    
+    
+    def ssim(self,
+              gt: np.ndarray, 
+              pred: np.ndarray,
+              use_population_range: Optional[bool] = False) -> float:
+        """
+        Compute Structural Similarity Index Metric (SSIM)
+    
+        Parameters
+        ----------
+        gt : np.ndarray
+            Ground truth
+        pred : np.ndarray
+            Prediction
+        use_population_range : bool, optional
+            When a predefined population wide dynamic range should be used.
+    
+        Returns
+        -------
+        ssim : float
+            strugtural similarity index metric.
+    
+        """
+        if use_population_range:
+            dynamic_range = self.dynamic_range
+        else:
+            dynamic_range = gt.max()-gt.min()
+            
+        ssim_value = structural_similarity(gt, pred, data_range=dynamic_range)
+        return float(ssim_value)
+    
